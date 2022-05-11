67c29dd18e0a1bdd03b2868efabdc1cfdf3dec0b
SmithaDRaj <99319437+SmithaDRaj@users.noreply.github.com>
2022-05-11 10:50:12 +0530

    Update1

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -3,8 +3,31 @@ package statisticker;
 import java.util.List;
 
 public class Statistics 
-{
+{   
+    float average;
+    float min;
+    float max;
+    public Statistics(average,min,max){
+        this.average = average;
+        this.min = min;
+        this.max = max;
+    }
     public static Stats getStatistics(List<___> numbers) {
         //implement the computation of statistics here
+        
+        int sum = 0;
+        int tempMax = 0;
+        int tempMin = numbers.get(0);
+        for (int i : numbers) {
+            sum+=i;
+            if (numbers.get(i) > tempMax){
+                tempMax = numbers.get(index);
+            }else if(numbers.get(i)<tempMin) {
+              tempMin = array[i];
+           }
+        }
+        this.max = sum/(float)numbers.size();
+        this.max = tempMax;
+        this.min = tempMin;
     }
 }
