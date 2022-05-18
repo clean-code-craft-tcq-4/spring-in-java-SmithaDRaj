2a3b7909e709e8d9962a3aefe9e63fd322d7332a
SmithaDRaj <99319437+SmithaDRaj@users.noreply.github.com>
2022-05-18 11:49:33 +0530

    Update4

--- src/main/java/statisticker/Statistics.java
+++ src/main/java/statisticker/Statistics.java
@@ -5,29 +5,34 @@ import java.util.List;
 public class Statistics 
 {   
     public class Stats{
-        float average = 0f;
-        float min = 0f;
-        float max = 0f;
+        float average = Float.NaN;
+        float min = Float.NaN;
+        float max = Float.NaN;
     }
     public static Stats getStatistics(List<> numbers) {
         //implement the computation of statistics here
-        
-        int sum = 0;
-        int tempMax = 0;
-        int tempMin = numbers.get(0);
-        for (int i : numbers) {
-            sum+=i;
-            if (numbers.get(i) > tempMax){
-                tempMax = numbers.get(index);
-            }else if(numbers.get(i)<tempMin) {
-              tempMin = array[i];
-           }
-        }
-        System.Out.Println("Average "+average+", Min "+min+", Max "+max);
         Stats s = new Stats();
-        s.average = sum/(float)numbers.size();
-        s.max = tempMax;
-        s.min = tempMin;
+        s.average = Float.NaN;
+        s.max = Float.NaN;
+        s.min = Float.NaN;
+        if(numbers.size() != 0){
+            
+            int sum = 0;
+            int tempMax = 0;
+            int tempMin = numbers.get(0);
+            for (int i : numbers) {
+                sum+=i;
+                if (numbers.get(i) > tempMax){
+                    tempMax = numbers.get(index);
+                }else if(numbers.get(i)<tempMin) {
+                    tempMin = array[i];
+               }
+            }
+            s.average = sum/(float)numbers.size();
+            s.max = tempMax;
+            s.min = tempMin;
+        }
+        System.Out.Println("Average "+s.average+", Min "+s.min+", Max "+s.max);
         return s;
     }
 }
