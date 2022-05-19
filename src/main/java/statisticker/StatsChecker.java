b6b2984adad76d9fa027155f372034d1318b4ec3
SmithaDRaj <99319437+SmithaDRaj@users.noreply.github.com>
2022-05-19 15:56:17 +0530

    Update StatsChecker.java

--- src/main/java/statisticker/StatsChecker.java
+++ src/main/java/statisticker/StatsChecker.java
@@ -1,1 +1,25 @@
+package statisticker;
 
+public class StatsChecker {
+	EmailAlert emailAlerter = new EmailAlert();
+  LEDAlert ledAlerter = new LEDAlert();
+  IAlerter alerters[] = {emailAlerter, ledAlerter};
+  float maxThreshold = 0f;
+  
+  public StatsChecker(maxThreshold, alerters){
+    this.maxThreshold = maxThreshold;
+    this.alerters = alerters;
+  }
+	public void checkAndAlert(List<Float> numbers) {
+    float max = numbers.get(0);
+    for (int i = 0; i < numbers.size(); i++) {
+        if (max < numbers.get(i))
+            max = numbers.get(i);
+    }
+    System.out.println("Max ="+ max);
+    if(max > maxThreshold){
+      emailAlerter.emailSent = True;
+      ledAlerter.ledGlows = True;
+    }
+	}
+}
