228bfa9686accc52116b31397b9728c94b126472
SmithaDRaj <99319437+SmithaDRaj@users.noreply.github.com>
2022-05-19 16:37:31 +0530

    Update StatsChecker.java

--- src/main/java/statisticker/StatsChecker.java
+++ src/main/java/statisticker/StatsChecker.java
@@ -3,16 +3,26 @@ package statisticker;
 import java.util.List;
 
 public class StatsChecker {
-	/*
- * EmailAlert emailAlerter = new EmailAlert(); LEDAlert ledAlerter = new
- * LEDAlert(); IAlerter alerters[] = null; float maxThreshold = 0f;
- * 
- * public StatsChecker(float maxThreshold, IAlerter[] alerters){
- * this.maxThreshold = maxThreshold; this.alerters = alerters; }
- * 
- * public void checkAndAlert(List<Float> numbers) { float max = numbers.get(0);
- * for (int i = 0; i < numbers.size(); i++) { if (max < numbers.get(i)) max =
- * numbers.get(i); } System.out.println("Max =" + max); if (max > maxThreshold)
- * { emailAlerter.emailSent = true; ledAlerter.ledGlows = true; } }
- */
+	EmailAlert emailAlerter = new EmailAlert();
+	LEDAlert ledAlerter = new LEDAlert();
+	IAlerter alerters[] = null;
+	float maxThreshold = 0f;
+
+	public StatsChecker(float maxThreshold, IAlerter[] alerters){
+	    this.maxThreshold = maxThreshold;
+	    this.alerters = alerters;
+	  }
+
+	public void checkAndAlert(List<Float> numbers) {
+		float max = numbers.get(0);
+		for (int i = 0; i < numbers.size(); i++) {
+			if (max < numbers.get(i))
+				max = numbers.get(i);
+		}
+		System.out.println("Max =" + max);
+		if (max > maxThreshold) {
+			alerters[0].emailSent = true;
+			alerters[1].ledGlows = true;
+		}
+	}
 }
